dac88f2218ae9b580aeecda21a991e763bca3fa9
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-11-05 08:33:00 -0300

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,25 @@
+// alert("opa!!!")
+// document.getElementById("texto").innerHTML="<h4>Judah Benjamin</h4> <b> JS </b>";
+// document.getElementById("cores").innerHTML="resultado";
+// alert("Fim de ano chegando!!!");
+// console.log("Não aparece na página!");
+
+// Variáveis JS
+
+var a,b,soma;
+let d;
+const g = 0;
+a = 3;
+b = 5;
+soma = a + b;
+document.write()
+
+/*
+Let x,y;
+x=6;
+y=2;
+valor=x/y;
+alert(valor)
+soma = (x>y) ? "Sim": "não";
+alert("Ola Mundo!")
+*/
